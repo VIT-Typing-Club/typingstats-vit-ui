8886d7cc21ea2461646b99beab50172f7d235e56
pk-vishnu <vishnuvpk05@gmail.com>
2026-03-27 23:41:07 +0530

    chore: apply formatter

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,28 @@
-import { useAuthStore } from "@/store/authStore";
-import { config } from "@/config";
+import { useAuthStore } from "@/store/authStore"
+import { config } from "@/config"
 
 export default function Navbar() {
-    const user = useAuthStore((s) => s.user);
-    const signOut = useAuthStore((s) => s.signOut);
+  const user = useAuthStore((s) => s.user)
+  const signOut = useAuthStore((s) => s.signOut)
 
-    const handleLogin = () => {
-        window.location.href = config.AUTH_LOGIN_URL;
-    };
+  const handleLogin = () => {
+    window.location.href = config.AUTH_LOGIN_URL
+  }
 
-    return (
-        <nav>
-            <div>VIT Typing Stats</div>
-            <div>
-                {user ? (
-                    <>
-                        <span>{user.displayName}</span>
-                        <button onClick={signOut}>Logout</button>
-                    </>
-                ) : (
-                    <button onClick={handleLogin}>Login with Discord</button>
-                )}
-            </div>
-        </nav>
-    );
+  return (
+    <nav>
+      <div>VIT Typing Stats</div>
+      <div>
+        {user ? (
+          <>
+            <span>{user.displayName}</span>
+            <span>{user.username}</span>
+            <button onClick={signOut}>Logout</button>
+          </>
+        ) : (
+          <button onClick={handleLogin}>Login with Discord</button>
+        )}
+      </div>
+    </nav>
+  )
 }
