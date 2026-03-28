4dce4865025ffed3daed191794cf1590e091408a
pk-vishnu <vishnuvpk05@gmail.com>
2026-03-28 16:39:06 +0530

    lock viewport

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import PublicProfilePage from "./pages/ProfilePage";
 
 function RootLayout() {
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <Navbar />
-      <main>
+      <main className="flex-1 min-h-0 overflow-hidden">
         <Outlet />
       </main>
     </div>
